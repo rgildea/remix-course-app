4a63371770cec0e36698afdda3a066a872f818f2
Ryan Gildea <ryangildea@gmail.com>
2023-03-22 20:39:58 -0400

    adding meta tags

--- app/routes/__marketing/pricing.jsx
+++ app/routes/__marketing/pricing.jsx
@@ -1,21 +1,21 @@
-import { FaTrophy, FaHandshake } from 'react-icons/fa';
+import { FaTrophy, FaHandshake } from "react-icons/fa";
 
-import PricingPlan from '~/components/marketing/PricingPlan';
+import PricingPlan from "~/components/marketing/PricingPlan";
 
 const PRICING_PLANS = [
   {
-    id: 'p1',
-    title: 'Basic',
-    price: 'Free forever',
-    perks: ['1 User', 'Up to 100 expenses/year', 'Basic analytics'],
-    icon: FaHandshake
+    id: "p1",
+    title: "Basic",
+    price: "Free forever",
+    perks: ["1 User", "Up to 100 expenses/year", "Basic analytics"],
+    icon: FaHandshake,
   },
   {
-    id: 'p2',
-    title: 'Pro',
-    price: '$9.99/month',
-    perks: ['Unlimited Users', 'Unlimited expenses/year', 'Detailed analytics'],
-    icon: FaTrophy
+    id: "p2",
+    title: "Pro",
+    price: "$9.99/month",
+    perks: ["Unlimited Users", "Unlimited expenses/year", "Detailed analytics"],
+    icon: FaTrophy,
   },
 ];
 
@@ -39,4 +39,9 @@ export default function PricingPage() {
   );
 }
 
-export function meta() {}
+export function meta() {
+  return {
+    title: "Pricing",
+    description: "See our pricing plans.",
+  };
+}
